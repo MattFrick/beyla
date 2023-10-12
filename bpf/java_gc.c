c662b4799a2077e84230943bb9d76254f41387e0
Matt Frick <matt.frick@grafana.com>
2023-10-11 22:09:09 -0700

    Add javagc.go, java_gc.c

---
+++ bpf/java_gc.c
@@ -0,0 +1,36 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "utils.h"
+#include "bpf_dbg.h"
+#include "ringbuf.h"
+
+#if 0
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, void *); // key: pointer to the request goroutine
+    __type(value, func_invocation);
+    __uint(max_entries, MAX_CONCURRENT_REQUESTS);
+} ongoing_GC SEC(".maps");
+#endif
+
+SEC("uprobe/mem__pool__gc__begin")
+int uprobe_MemPoolGcBegin(struct pt_regs *ctx) {
+    bpf_dbg_printk("=== uprobe/MemPoolGcBegin === ");
+    return 0;
+}
+
+SEC("uprobe/mem__pool__gc__end")
+int uprobe_MemPoolGcEnd(struct pt_regs *ctx) {
+    bpf_dbg_printk("=== uprobe/MemPoolGcEnd === ");
+    return 0;
+}
---
+++ pkg/internal/ebpf/javagc/javagc.go
@@ -0,0 +1,96 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package goruntime
+
+import (
+	"context"
+	"io"
+	"log/slog"
+
+	"github.com/cilium/ebpf"
+
+	ebpfcommon "github.com/grafana/beyla/pkg/internal/ebpf/common"
+	"github.com/grafana/beyla/pkg/internal/exec"
+	"github.com/grafana/beyla/pkg/internal/goexec"
+	"github.com/grafana/beyla/pkg/internal/imetrics"
+	"github.com/grafana/beyla/pkg/internal/request"
+	"github.com/grafana/beyla/pkg/internal/svc"
+)
+
+//go:generate $BPF2GO -cc $BPF_CLANG -cflags $BPF_CFLAGS -target amd64,arm64 bpf ../../../../bpf/java_gc.c -- -I../../../../bpf/headers
+//go:generate $BPF2GO -cc $BPF_CLANG -cflags $BPF_CFLAGS -target amd64,arm64 bpf_debug ../../../../bpf/java_gc.c -- -I../../../../bpf/headers -DBPF_DEBUG
+
+type Tracer struct {
+	Cfg        *ebpfcommon.TracerConfig
+	Metrics    imetrics.Reporter
+	bpfObjects bpfObjects
+	closers    []io.Closer
+}
+
+func (p *Tracer) Load() (*ebpf.CollectionSpec, error) {
+	loader := loadBpf
+	if p.Cfg.BpfDebug {
+		loader = loadBpf_debug
+	}
+	return loader()
+}
+
+func (p *Tracer) Constants(_ *exec.FileInfo, _ *goexec.Offsets) map[string]any {
+	return make(map[string]any)
+}
+
+func (p *Tracer) BpfObjects() any {
+	return &p.bpfObjects
+}
+
+func (p *Tracer) AddCloser(c ...io.Closer) {
+	p.closers = append(p.closers, c...)
+}
+
+func (p *Tracer) GoProbes() map[string]ebpfcommon.FunctionPrograms {
+	return nil
+}
+
+func (p *Tracer) KProbes() map[string]ebpfcommon.FunctionPrograms {
+	return nil
+}
+
+func (p *Tracer) UProbes() map[string]map[string]ebpfcommon.FunctionPrograms {
+	return map[string]map[string]ebpfcommon.FunctionPrograms{
+		"libjvm.so": {
+			"mem__pool__gc__begin": {
+				Required: false,
+				Start:    p.bpfObjects.UprobeMemPoolGcBegin,
+			},
+			"mem__pool__gc__end": {
+				Required: false,
+				Start:    p.bpfObjects.UprobeMemPoolGcEnd,
+			},
+		},
+	}
+}
+
+func (p *Tracer) SocketFilters() []*ebpf.Program {
+	return nil
+}
+
+func (p *Tracer) Run(ctx context.Context, eventsChan chan<- []request.Span, service svc.ID) {
+	logger := slog.With("component", "goruntime.Tracer")
+	ebpfcommon.ForwardRingbuf[ebpfcommon.HTTPRequestTrace](
+		service,
+		p.Cfg, logger, p.bpfObjects.Events,
+		ebpfcommon.ReadHTTPRequestTraceAsSpan,
+		p.Metrics,
+		append(p.closers, &p.bpfObjects)...,
+	)(ctx, eventsChan)
+}
